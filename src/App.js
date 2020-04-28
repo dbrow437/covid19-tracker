f406ed0787337b45421dbacaf7a825948f1c400c
Damon Brown <dbrow437@gmail.com>
2020-04-27 20:24:42 -0700

    chart created

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import { fetchData } from "./api";
 export default class App extends Component {
 	state = {
 		data: {},
+		country: '',
 	}
 	async componentDidMount() {
 		const fetchedData = await fetchData();
 
 		this.setState({ data: fetchedData });
+	}
+
+	handleCountryChange = async (country) => {
+		console.log(country)
 	}
 	render() {
 		const { data } = this.state;
